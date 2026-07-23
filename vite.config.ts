b0598c293a9d3903851df19fc1dff08dd4d47756
Franco <fdg2112@gmail.com>
2026-07-23 00:16:18 -0300

    Publicar sitio con GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/logiajorgewashington44/',
   plugins: [react(), tailwindcss()],
 })
