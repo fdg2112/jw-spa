a021771c88db81d69550f70c6abd8a1d96994617
fdg2112 <fdg2112@gmail.com>
2026-07-23 10:06:14 -0300

    cambio naming

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/logiajorgewashington44/',
+  base: '/jw-spa/',
   build: {
     outDir: 'docs',
   },
